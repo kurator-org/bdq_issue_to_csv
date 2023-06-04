8522bf5c917e37a15648b3d47ef731f07fe1dea3
chicoreus <mole@morris.net>
2023-06-04 18:16:40 -0400

    Incrementing version to 0.0.4-SNAPSHOT to reflect addition of date last modified.

--- getAllIssues.sh
+++ getAllIssues.sh
@@ -5,4 +5,4 @@ wget "https://api.github.com/repos/tdwg/bdq/issues?labels=TEST&per_page=100&page
 
 jq -s 'flatten | group_by(.id) | map(reduce .[] as $x ({}; . * $x))' issuelist1.json issuelist2.json > issuelist.json
 
-java -jar target/issueconverter-0.0.3-SNAPSHOT-jar-with-dependencies.jar -f issuelist.json
+java -jar target/issueconverter-0.0.4-SNAPSHOT-jar-with-dependencies.jar -f issuelist.json
