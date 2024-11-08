4b3023f27558d7a29b7dc63feb207973a886708a
chicoreus <mole@morris.net>
2024-11-08 13:52:52 -0500

    Incrementing version to 1.0.1-SNAPSHOT for ongoing work, in pom, scripts, readme and code.

--- make_test_csv.sh
+++ make_test_csv.sh
@@ -9,7 +9,7 @@ if [ $ok = "true" ]; then
 	wget "https://api.github.com/repos/tdwg/bdq/issues?labels=CORE&per_page=100"  -O issuelist1.json
 	wget "https://api.github.com/repos/tdwg/bdq/issues?labels=CORE&per_page=100&page=2" -O issuelist2.json
 	jq -s 'flatten | group_by(.id) | map(reduce .[] as $x ({}; . * $x))' issuelist1.json issuelist2.json > issuelist.json
-	java -jar ./target/issueconverter-1.0.0-jar-with-dependencies.jar -f issuelist.json -u ../bdq/tg2/core/usecase_test_list.csv -l ../bdq/tg2/core/test_label_mappings.csv -g ../bdq/tg2/core/TG2_tests_additional_guids.csv -a ../bdq/tg2/core/TG2_tests_argument_guids.csv
+	java -jar ./target/issueconverter-1.0.1-SNAPSHOT-jar-with-dependencies.jar -f issuelist.json -u ../bdq/tg2/core/usecase_test_list.csv -l ../bdq/tg2/core/test_label_mappings.csv -g ../bdq/tg2/core/TG2_tests_additional_guids.csv -a ../bdq/tg2/core/TG2_tests_argument_guids.csv
 	cd ../
 	cp bdq_issue_to_csv/output.csv bdq/tg2/core/TG2_tests.csv
 	cp bdq_issue_to_csv/multirecord_measures.csv bdq/tg2/core/TG2_multirecord_measure_tests.csv
@@ -25,7 +25,7 @@ if [ $ok = "true" ]; then
     # Supplementary Tests
     cd ../bdq_issue_to_csv
     wget "https://api.github.com/repos/tdwg/bdq/issues?labels=Supplementary&per_page=100&state=all" -O supplementalissuelist.json
-    java -jar target/issueconverter-1.0.0-jar-with-dependencies.jar -f supplementalissuelist.json
+    java -jar target/issueconverter-1.0.1-SNAPSHOT-jar-with-dependencies.jar -f supplementalissuelist.json
     cp ./output.csv ../bdq/tg2/supplementary/TG2_supplementary_tests.csv
     cd ../kurator-ffdq
     grep -v "AllDarwin" ../bdq/tg2/supplementary/TG2_supplementary_tests.csv  > data/TG2_supplementary_tests.csv
