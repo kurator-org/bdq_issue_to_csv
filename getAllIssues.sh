7e43bb94b0456d4e592db156486bca2e424a018b
chicoreus <mole@morris.net>
2024-11-08 13:37:38 -0500

    Updating version in another script.

--- getAllIssues.sh
+++ getAllIssues.sh
@@ -5,12 +5,12 @@ wget "https://api.github.com/repos/tdwg/bdq/issues?labels=TEST&per_page=100&page
 
 jq -s 'flatten | group_by(.id) | map(reduce .[] as $x ({}; . * $x))' issuelist1.json issuelist2.json > issuelist.json
 
-java -jar target/issueconverter-0.0.5-SNAPSHOT-jar-with-dependencies.jar -f issuelist.json
+java -jar target/issueconverter-1.0.0-jar-with-dependencies.jar -f issuelist.json
 
 cp output.csv ../bdq/tg2/core/TG2_tests.csv
 
 wget "https://api.github.com/repos/tdwg/bdq/issues?labels=Supplementary&per_page=100&state=all" -O supplementalissuelist.json
 
-java -jar target/issueconverter-0.0.5-SNAPSHOT-jar-with-dependencies.jar -f supplementalissuelist.json
+java -jar target/issueconverter-1.0.0-jar-with-dependencies.jar -f supplementalissuelist.json
 
 cp output.csv ../bdq/tg2/supplementary/TG2_supplementary_tests.csv
